4b17a74ba7b12cea93e60e15ded607c4ccd1d622
Cleve-codes <clevemomanyi@gmail.com>
2024-07-02 23:17:48 +0300

    Implemented the Blog section and switched most components to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 
+import cloudflare from '@astrojs/cloudflare';
+
 
 // https://astro.build/config
 export default defineConfig({
   renderers: [svelte()],
-  integrations: [svelte()]
+  integrations: [svelte()],
+  output: 'server',
+  adapter: cloudflare()
 });
