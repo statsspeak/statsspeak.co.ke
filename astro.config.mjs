06d1fa1680d3dcf3d0ab6fa1812e9f652e579eee
Alloys Mila <mofax@users.noreply.github.com>
2024-07-29 08:07:25 +0300

    format with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  renderers: [svelte()],
-  integrations: [svelte()],
-  output: 'server',
-  adapter: cloudflare()
+	renderers: [svelte()],
+	integrations: [svelte()],
+	output: "server",
+	adapter: cloudflare(),
 });
