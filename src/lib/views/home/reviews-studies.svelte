549c22a1045b61d289bf24c1d296c086da44dcb8
Cleve-codes <clevemomanyi@gmail.com>
2024-09-11 13:31:31 +0300

    [UI] responsive resources page

--- src/lib/views/home/reviews-studies.svelte
+++ src/lib/views/home/reviews-studies.svelte
@@ -1,17 +1,14 @@
 <script>
-  import Carousel from 'svelte-carousel'
-
   const images = [
-  '/case-studies/case-1.jpg',
-  '/case-studies/MoH.png',
-  '/case-studies/case-3.jpg',
-  '/case-studies/amref.jpg',
-  '/case-studies/case-4.jpg'
+    '/case-studies/case-1.jpg',
+    '/case-studies/MoH.png',
+    '/case-studies/case-3.jpg',
+    '/case-studies/amref.jpg',
+    '/case-studies/case-4.jpg'
   ];
-  </script>
+</script>
 
-
-  <div id="our-team" class="inline-container">
+<div id="our-team" class="inline-container">
   <div class="heading-section">
     <div>
       <div class="title">Case Studies</div>
@@ -29,104 +26,70 @@
       </div>
     {/each}
   </div>
-  {#if Carousel}
-    <!-- Display carousel on small screens -->
-     <div class="grid-carousel">
-      <Carousel
-        autoplay
-        itemsToShow={1}
-        loop={true}
-        navigation={true}
-        pagination={true}
-        infinite={true}
-        auto={false}
-      >
-        {#each images as image, index}
-          <div class="grid-item" key={index}>
-            <a href="/resources/case-studies">
-              <img src={image} alt={'Case Study ' + (index + 1)} />
-              <div class="overlay">
-                <div class="overlay-text">Read Case Study</div>
-              </div>
-            </a>
-          </div>
-        {/each}
-      </Carousel>
-    </div>
-  {/if}
-  </div>
+</div>
 
-  <style>
 
+<style>
   #our-team {
-    margin-top: 10%;
-    /* margin-inline: auto; */
-  }
+  margin-top: 10%;
+}
 
-  .heading-section {
+.heading-section {
   margin-top: 3em;
   display: flex;
-  /* padding-bottom: 112px; */
-  }
+}
 
-  .heading-section div h4{
-    color: var(--description-text)
-  }
+.heading-section div h4 {
+  color: var(--description-text);
+}
 
-  .title {
+.title {
   font-family: var(--font-family-secondary), "Source Sans 3", sans-serif;
   color: var(--text-color);
   margin-bottom: 14px;
   font-size: 2em;
   font-weight: 600;
-  /* margin-left: -80%; */
-  /* margin-top: .5em; */
-  /* border: 1px solid red; */
-  }
+}
 
-  .description {
+.description {
   margin-left: 3em;
   margin-top: 3em;
-  }
+}
 
-  .grid-gallery {
-  /* border: 1px solid red; */
-  min-width: 100%;
-  /* m-width: 1400px; */
-  /* margin: 0 auto; */
-  margin-left: -2%;
+.grid-gallery {
+  width: 100%;
   padding: 50px 20px;
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: 170px 220px 170px;
   grid-gap: 16px;
-  }
+  /* grid-template-columns: 1fr;
+  grid-template-rows: repeat(auto-fit, minmax(250px, 1fr)); */
+}
 
-  .grid-gallery .grid-item {
+.grid-gallery .grid-item {
   position: relative;
   background-color: #efefef;
   overflow: hidden;
   border-radius: 10px;
-  min-width: 500px;
   /* border: 1px solid red; */
-  }
+  /* max-height: 500px; */
+}
 
-  .grid-gallery .grid-item img {
+.grid-gallery .grid-item img {
   width: 100%;
   height: 100%;
-  object-fit: fill; /* Ensure the image fills the grid item */
+  /* object-fit: cover; */
   transition: transform 0.5s cubic-bezier(0.165, 0.84, 0.44, 1);
-  }
+}
 
-  .grid-gallery .grid-item:hover img {
+.grid-gallery .grid-item:hover img {
   transform: scale(1.1);
-  }
+}
 
-  .grid-gallery .grid-item a {
-  cursor: zoom-in;
-  }
+.grid-gallery .grid-item a {
+  cursor: pointer;
+}
 
-  .grid-gallery .grid-item .overlay {
+.grid-gallery .grid-item .overlay {
   position: absolute;
   top: 0;
   left: 0;
@@ -138,161 +101,92 @@
   align-items: center;
   opacity: 0;
   transition: opacity 0.5s;
-  }
+}
 
-  .grid-gallery .grid-item .overlay-text {
+.grid-gallery .grid-item .overlay-text {
   color: white;
   font-size: 1.5em;
   text-align: center;
-  }
+}
 
-  .grid-gallery .grid-item:hover .overlay {
+.grid-gallery .grid-item:hover .overlay {
   opacity: 1;
-  }
+}
 
-  /* First item spans two rows */
-  .grid-gallery .grid-item:nth-child(1) {
-  grid-column: 1 / 2; /* Occupies the first column */
-  grid-row: 1 / span 2; /* Spans from the first to the third row (occupies three rows) */
-  border-right: 4px solid rgb(140, 140, 140); /* 2px solid border on the right */
-  border-bottom:4px solid rgb(140, 140, 140); /* 2px solid border on the bottom */
-  border-left: 1px solid rgb(140, 140, 140);
-  border-top: .5px solid rgb(140, 140, 140)
-  }
-
-  /* .grid-gallery .grid-item:nth-child(1):hover {
-    border: none;
-  } */
-
-  /* Second item */
-  .grid-gallery .grid-item:nth-child(2) {
-  grid-column: 1 / 2; /* Occupies the second column */
-  grid-row: 3 / span 1; /* Spans the first row only */
-  }
-
-  /* Third item spans two rows */
-  .grid-gallery .grid-item:nth-child(3) {
-  grid-column: 2 / 3; /* Occupies the second column */
-  grid-row: 2 / span 2; /* Spans the second and third rows */
-  }
-
-  .grid-gallery .grid-item:nth-child(4) {
-    /* grid-column: 3 / 4;
-    grid-row: 1 / span 2; */
-    border-left: 1px solid rgb(140, 140, 140);
-    border-right: 4px solid rgb(140, 140, 140);
-    border-top: .5px solid rgb(140, 140, 140);
-    border-bottom: 4px solid rgb(140, 140, 140);
-    }
-
-    .grid-gallery .grid-item:nth-child(4) img {
-      object-fit: cover;
-    }
-
-    /* .grid-gallery .grid-item:nth-child(4):hover {
-      border: none;
-    } */
-
-    .grid-gallery .grid-item:nth-child(5) {
-  grid-column: 3 / 4;
-  grid-row: 1 / span 3;
-  }
-
-  .grid-carousel {
-  display: none;
-  }
-
-  /* Media Queries */
-  @media (max-width: 1200px) {
+/* Stacked layout for smaller screens */
+@media (max-width: 767px) {
   .grid-gallery {
-    display: none;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(auto-fit, minmax(250px, 1fr));
   }
 
-  .grid-carousel {
-    display: block;
-    margin-top: 2em;
+  .grid-gallery .grid-item {
+    height: 250px;
+    /* border: 1px solid red; */
   }
 
-  .grid-carousel .grid-item {
-    max-height: 300px;
-    padding: .5em;
-  }
-
-  .grid-carousel .grid-item img {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-    border-radius: 15px;
-  }
-  }
-
-  @media (max-width: 768px) {
   .heading-section {
     flex-direction: column;
     align-items: center;
     text-align: center;
   }
+}
 
-  .description {
-    margin-left: 0;
-    margin-top: 1em;
-  }
-
+/* Grid layout for larger screens */
+@media (min-width: 1024px) {
   .grid-gallery {
-    display: none;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: 170px 220px 170px;
   }
 
-  .grid-carousel {
-    display: block;
-    margin-top: 2em;
+  .grid-gallery .grid-item {
+    object-fit: cover;
+    /* min-height: 200px; */
   }
 
-  .grid-carousel .grid-item {
-    max-height: 300px;
-    padding: .5em;
+  .grid-gallery .grid-item:nth-child(1) {
+    grid-column: 1 / 2;
+    grid-row: 1 / span 2;
+    border-right: 4px solid rgb(140, 140, 140);
+    border-bottom: 4px solid rgb(140, 140, 140);
+    border-left: 1px solid rgb(140, 140, 140);
+    border-top: 0.5px solid rgb(140, 140, 140);
   }
 
-  .grid-carousel .grid-item img {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-    border-radius: 15px;
-  }
+  .grid-gallery .grid-item:nth-child(2) {
+    grid-column: 1 / 2;
+    grid-row: 3 / span 1;
   }
 
-  @media (max-width: 480px) {
+  .grid-gallery .grid-item:nth-child(3) {
+    grid-column: 2 / 3;
+    grid-row: 2 / span 2;
+  }
+
+  .grid-gallery .grid-item:nth-child(4) {
+    border-left: 1px solid rgb(140, 140, 140);
+    border-right: 4px solid rgb(140, 140, 140);
+    border-top: 0.5px solid rgb(140, 140, 140);
+    border-bottom: 4px solid rgb(140, 140, 140);
+  }
+
+  .grid-gallery .grid-item:nth-child(5) {
+    grid-column: 3 / 4;
+    grid-row: 1 / span 3;
+  }
+}
+
+@media (max-width: 480px) {
   .title {
     font-size: 1.2em;
   }
 
-  .description {
-    font-size: 1em;
-  }
-
-  .grid-gallery {
-    display: none;
-  }
-
-  .grid-carousel {
-    display: block;
-    margin-top: 2em;
-  }
-
-  .grid-carousel .grid-item {
-    max-height: 300px;
-    padding: .5em;
-  }
-
-  .grid-carousel .grid-item img {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-    border-radius: 15px;
+  .grid-gallery .grid-item {
+    height: 200px;
   }
 
   .grid-gallery .grid-item .overlay-text {
     font-size: 1em;
   }
-  }
-
-  </style>
+}
+</style>
